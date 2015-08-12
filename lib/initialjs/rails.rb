471fa043753b0aaa71d32d2998b27b3ac818efce
David Gil <dgilperez@gmail.com>
2015-08-12 17:42:08 +0200

    Initial commit

---
+++ lib/initialjs/rails.rb
@@ -0,0 +1,7 @@
+require "initialjs/rails/version"
+
+module Initialjs
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/initialjs/rails/version.rb
@@ -0,0 +1,5 @@
+module Initialjs
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
